67777d375a07e146e0fd64f7b1fef97d074fa873
a-pinchuk <co.pinchuk@gmail.com>
2022-03-13 22:51:05 +0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://zabobon.com/${category}/4`);
+12312313
+
+let incr = 10,
+    dect = 10;
+
+// incr++;
+// dect--;
+
+console.log(++incr);
+
+console.log(--dect);
+
+console.log(2 + 2 * 2 !== '6');
+
+const   isChecked = true,
+        isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// let arra = ['pul', 'asd', 5, '123'];
+// console.log(arra[2]);
+
+// alert("hello");
+
+// // const result = confirm('are u here>');
+// // console.log(result);
+
+// const answer = prompt('How old are u?', "18");
+// console.log(answer);
+
+// let massiv = [];
+
+// massiv[0] = prompt('How old are u?', "");
+// massiv[1] = prompt('How name is?', "");
+// massiv[2] = prompt('privet?', "");
+
+// document.write(massiv);
